782c35502f2a84ec0c732c513056ed333798fdfb
CivlisedOctopus <105605472+CivlisedOctopus@users.noreply.github.com>
2023-12-07 20:16:26 -0500

    updated finder and the filter in backend, fixed update pet

--- P3/frontend/petpal/src/pages/UpdatePet/index.jsx
+++ P3/frontend/petpal/src/pages/UpdatePet/index.jsx
@@ -3,8 +3,12 @@ import { useContext, useState, useEffect } from "react";
 import { Outlet, Link, useLocation } from "react-router-dom"
 //import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useParams } from 'react-router-dom';
 
 const CreatePet = () => {
+    const [pet, setPet] = useState(null);
+        const { petId } = useParams();
+
     const [pic, setPic] = useState(null);
     const [name, setName] = useState('');
     const [location, setLocation] = useState('');
@@ -16,7 +20,26 @@ const CreatePet = () => {
     const [description, setDescription] = useState('');
 
 
+   useEffect(() => {
+       const fetchPet = async () => {
+         try {
+           // Fetch pet details
+           const petResponse = await fetch(`http://127.0.0.1:8000/pets/details/${petId}/`, {
+                 method: 'GET',
+                 headers: {
+                   'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+                 }
+               });
+           const petData = await petResponse.json();
+           setPet(petData);
 
+         } catch (error) {
+           console.error('Error fetching pet details:', error);
+         }
+       };
+
+       fetchPet();
+     }, [petId]);
    const submit = async (e) => {
         e.preventDefault();
         const pet = {
@@ -34,7 +57,7 @@ const CreatePet = () => {
 
         try {
             console.log(pet);
-            const response = await fetch('http://127.0.0.1:8000/pets/newpet/', {
+            const response = await fetch('http://127.0.0.1:8000/pets/${petId}/', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
@@ -45,6 +68,7 @@ const CreatePet = () => {
 
             const data = await response.json();
             console.log('Pet created successfully:', data);
+            window.location.href = '/'
             // You can redirect to the pet list page or perform other actions here
           } catch (error) {
             console.error('Error creating pet:', error);
@@ -60,14 +84,16 @@ const CreatePet = () => {
             // You can perform further actions with the file, such as uploading it to a server.
           }
         };
-
+    if(!pet){
+    return(<p>Loading...</p>);
+    }
     return (<body>
 
     <section className="adoptapp-background">
       <div className="container centered">
         <div className="col-md-6">
           <h2 className="mb-4 text-center">
-            <strong>Pet Creation Form</strong>
+            <strong>Pet Edit Form</strong>
           </h2>
 
           <form className="row g-3 needs-validation" onSubmit={submit}>
@@ -82,7 +108,7 @@ const CreatePet = () => {
                     <label htmlFor="petName" className="form-label">
                       <strong>Pet Name</strong>
                     </label>
-                    <input type="text" className="form-control" id="petName" required value={name} onChange={e => setName(e.target.value)}/>
+                    <input type="text" className="form-control" id="petName" required value={name} placeholder={pet.name} onChange={e => setName(e.target.value)}/>
                     <div className="invalid-feedback">Please provide a pet name.</div>
                   </div>
 
@@ -90,7 +116,7 @@ const CreatePet = () => {
                     <label htmlFor="petLocation" className="form-label">
                       <strong>Location</strong>
                     </label>
-                    <input type="text" className="form-control" id="Location" required value={location} onChange={e => setLocation(e.target.value)}/>
+                    <input type="text" className="form-control" id="Location" required value={location} placeholder={pet.location} onChange={e => setLocation(e.target.value)}/>
                     <div className="invalid-feedback">Please provide a location.</div>
                   </div>
 
@@ -98,9 +124,9 @@ const CreatePet = () => {
                     <label htmlFor="gender" className="form-label">
                         <strong>Gender</strong>
                     </label>
-                    <select className="form-select" id="gender" required value={gender} onChange={e => setGender(e.target.value)}>
+                    <select className="form-select" id="gender" required value={gender} placeholder={pet.gender} onChange={e => setGender(e.target.value)}>
                     <option selected disabled value="">
-                        Choose...
+                        Choose... ({pet.gender})
                         </option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
@@ -112,7 +138,7 @@ const CreatePet = () => {
                     <label htmlFor="age" className="form-label">
                       <strong>Age</strong>
                     </label>
-                    <input type="number" className="form-control" id="age" required value={age} onChange={e => setAge(e.target.value)}/>
+                    <input type="number" className="form-control" id="age" required value={age}  onChange={e => setAge(e.target.value)}/>
                     <div className="invalid-feedback">Please provide a valid age.</div>
                   </div>
 
@@ -120,9 +146,9 @@ const CreatePet = () => {
                     <label htmlFor="gender" className="form-label">
                         <strong>Size</strong>
                     </label>
-                    <select className="form-select" id="size" required value={size} onChange={e => setSize(e.target.value)}>
+                    <select className="form-select" id="size" required value={size} placeholder={pet.size} onChange={e => setSize(e.target.value)}>
                     <option selected disabled value="">
-                        Choose...
+                        Choose... ({pet.size})
                         </option>
                         <option value="small">Small</option>
                         <option value="medium">Medium</option>
@@ -134,14 +160,14 @@ const CreatePet = () => {
                     <label htmlFor="Breed" className="form-label">
                       <strong>Breed</strong>
                     </label>
-                    <input type="text" className="form-control" id="breed" required value={breed} onChange={e => setBreed(e.target.value)}/>
+                    <input type="text" className="form-control" id="breed" required value={breed} placeholder={pet.breed} onChange={e => setBreed(e.target.value)}/>
                     <div className="invalid-feedback">Please provide a Breed.</div>
                   </div>
                   <div className="col-md-12">
                     <label htmlFor="color" className="form-label">
                       <strong>Color</strong>
                     </label>
-                    <input type="text" className="form-control" id="color" required value={color} onChange={e => setColor(e.target.value)}/>
+                    <input type="text" className="form-control" id="color" required value={color} placeholder={pet.color} onChange={e => setColor(e.target.value)}/>
                     <div className="invalid-feedback">Please provide a Color.</div>
                   </div>
                   </div>
@@ -154,12 +180,12 @@ const CreatePet = () => {
                 <h5>
                   <strong>Description</strong>
                 </h5>
-                <textarea className="form-control mb-3" id="allergies1" rows="3" required value={description} onChange={e => setDescription(e.target.value)}></textarea>
+                <textarea className="form-control mb-3" id="allergies1" rows="3" required value={description} placeholder={pet.description} onChange={e => setDescription(e.target.value)}></textarea>
               </div>
             </div>
 
-            <button type="submit" className="btn btn-primary mt-3" href="../../finder/">
-              Create Pet
+<button type="submit" className="btn btn-primary mt-3">
+              Edit Pet
             </button>
           </form>
         </div>
